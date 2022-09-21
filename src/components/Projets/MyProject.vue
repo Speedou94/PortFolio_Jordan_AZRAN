0c122f547892f3c8e424bff3d4c940c1c18cd6f7
Speedou <j.azran23@gmail.com>
2022-09-21 20:21:45 +0200

    mise en page

--- src/components/Projets/MyProject.vue
+++ src/components/Projets/MyProject.vue
@@ -4,7 +4,7 @@
   </div>
   <br><br>
   <div class="project">
-  <div class="card container is-flex-direction-column">
+  <div class="card container mt-3">
     <header class="card-header">
       <p class="card-header-title">
         ENI-Enchères
@@ -24,7 +24,7 @@
     </footer>
   </div>
   <br><br>
-  <div class="card container">
+    <div class="card container mt-3">
     <header class="card-header">
       <p class="card-header-title">
         LCDE Photo </p>
@@ -44,7 +44,7 @@
     </footer>
   </div>
   <br><br>
-  <div class="card container">
+    <div class="card container mt-3">
     <header class="card-header">
       <p class="card-header-title">
         PortFolio Jordan Azran </p>
@@ -64,14 +64,35 @@
       <a href="https://github.com/Speedou94/PortFolio_Jordan_AZRAN" class="card-footer-item">Repository GitHub</a>
     </footer>
   </div>
+    <div class="card container mt-3">
+      <header class="card-header">
+        <p class="card-header-title">
+          ENI-Sortie </p>
+      </header>
+      <div class="card-content">
+        <p class="image is-128x128">
+          <img src="../../assets/img/Various/logoenisortir.png">
+        </p>
+        <div class="content">
+          Je vous partage également le code de mon portefolio.<br>
+          Créer en VueJs avec un backOffice en NodeJs.<br>
+          Il est en constant évolution, n'hésitez pas à le consulter de temps en temps pour voir les améliorations.
+          <br>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="https://github.com/Speedou94/PortFolio_Jordan_AZRAN" class="card-footer-item">Repository GitHub</a>
+      </footer>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
 .project{
-  display: -webkit-box;
-}
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;}
 
 .image {
   height: 128px;
